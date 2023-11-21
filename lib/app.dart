636c2792ab7be276935595b869ef568dd1294c7a
Sharada Marasinha <86095574+sharada-marasinha@users.noreply.github.com>
2023-11-21 22:01:06 +0530

    add sum windows

--- lib/app.dart
+++ lib/app.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:roller_dice/gradient_container.dart';
-import 'package:roller_dice/roller_dicer.dart';
 
 class RollediceApp extends StatelessWidget {
   const RollediceApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    RollerDice rollerDice = const RollerDice();
     return const MaterialApp(
       home: Scaffold(
         body: GradientContainer(),
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:roller_dice/app.dart';
-
 void main() {
   runApp(const RollediceApp());
 }
